b98e0ca0836cfe373db86f59e1f17d194b2cbd81
Kavindu-Madhushanka <madu10275@gmail.com>
2026-05-03 23:49:56 +0530

    add app.py

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify
+import pickle
+
+app = Flask(__name__)
+
+# load model and vectorizer
+model = pickle.load(open("/model.pkl", "rb"))
+vectorizer = pickle.load(open("/vectorizer.pkl", "rb"))
+
+@app.route("/")
+def home():
+    return "Fake Email Detection API is running ✔"
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    try:
+        data = request.get_json()
+
+        email_text = data["text"]
+
+        # preprocess (same as training)
+        email_text = email_text.replace("Subject:", "")
+
+        # transform
+        vect_text = vectorizer.transform([email_text])
+
+        # predict
+        prediction = model.predict(vect_text)[0]
+
+        return jsonify({
+            "prediction": str(prediction)
+        })
+
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+if __name__ == "__main__":
+    app.run(debug=True)
